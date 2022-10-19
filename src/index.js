4bc3cd50c0152c560c48aa4cf7adc0eaf50faeca
GaryBeilman <lsu20041000@yahoo.com>
2022-10-18 20:28:52 -0500

    started on index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// testing
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FitnessTracker Project</title>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
