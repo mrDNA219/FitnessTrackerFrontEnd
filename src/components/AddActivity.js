ab7dc0703dd166c9d6e81b1bebfc6af271be2b28
Francis Harbourt <nharbourt@gmail.com>
2022-10-29 10:54:19 -0400

    added addactivity to addactivity.js

--- src/components/AddActivity.js
+++ src/components/AddActivity.js
@@ -1,1 +1,34 @@
 import react from "react";
+const AddActivity = (routineId, activities, getMyRoutinesHelper) => {
+    const [currentActivity, setCurrentActivity] = useState([]);
+  
+    const {id, count, duration } = currentActivity;
+    console.log(activities)
+    async function joinActivityToRoutine(){
+      const activityToAdd = {
+        activityId: id,
+        count: count,
+        duration: duration
+      }
+      await addActivityToRoutine(routineId, activityToAdd);
+      getMyRoutinesHelper();
+    }
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        joinActivityToRoutine();
+      }}>
+        <fieldset>
+        <select id="activity-dropdown" value={currentActivity} onChange={e => setCurrentActivity(e.target.value)}>
+          {
+            activities.map((activity) => {
+              return <option value={activity}>{activity.name}</option>
+            })
+          }
+        </select>
+        </fieldset>
+        <button type="submit">Add Activity</button>
+      </form>
+    )
+  }
+  export default AddActivity
